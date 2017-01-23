4233af8ba52b54b784a25cfb0e676fc4e62186a5
Patrick K <patrickkim93@gmail.com>
2017-01-23 13:37:02 -0500

    Change font, add job description and welcome msg

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,16 +1,16 @@
 var bio = {
   "name" : "Patrick",
   "role" : "Web Developer",
-  "welcome" : "Hi there! Thanks for viewing my interactive resume. I am a web developer currently seeking employment.",
+  "welcomeMessage" : "Hi there! Thanks for viewing my interactive resume. I am a web developer currently seeking employment. Willing to relocate for great opportunities!",
   "contacts" : {
-    "mobile" : "(111) 111-1111",
+    "mobile" : "(919) 818-0165",
     "email" : "patrickkim93@gmail.com",
     "github" : "patrickk93",
-    "linkedin" : "https://linkedin.com/",
+    "linkedin" : "https://www.linkedin.com/in/patrickk93",
     "location" : "Raleigh, NC"
   },
   "biopic" : "url",
-  "skills" : ["HTML", "CSS", "JavaScript", "ReactJS", "jQuery", "ExpressJS", "NodeJS", "MongoDB", "Bootstrap"]
+  "skills" : ["HTML", "CSS", "JavaScript", "ReactJS", "jQuery", "ExpressJS", "NodeJS", "PostgreSQL", "Bootstrap"]
 };
 
 var work = {
@@ -20,56 +20,63 @@ var work = {
       "title" : "Freelancer",
       "location" : "South Korea",
       "dates" : "09/2016 ~ 12/2016",
-      "description" : ["Translate research papers from Korean to English for clients",
-                       " Prepare adults for interviewing skills in English",
-                       " Instruct adults to write effective cover letters and resumes",
-                       " Teach conversational English to high school students and adults"
-                      ]
+      "description" : "- Translate research papers from Korean to English for clients" + "<br>"+
+                       "- Prepare adults for interviewing skills in English"+"<br>"+
+                       "- Instruct adults to write effective cover letters and resumes"+"<br>"+
+                       "- Teach conversational English to high school students and adults"
+
     },
     {
       "employer" : "Gyeonggido Board of Education",
       "title" : "Native English Teacher",
       "location" : "Pyeongtaek, South Korea",
       "dates" : "09/2015 ~ 09/2016",
-      "description" : ["Instruct 3rd - 6th grade public school students in English",
-                       " Brainstorm new materials and ideas with coteachers",
-                       " Translate documents in English to Korean and vice versa",
-                       " Act as the translator between administrative staff and US military personnel"
-                      ]
+      "description" : "- Instruct 3rd - 6th grade public school students in English"+"<br>"+
+                       "- Brainstorm new materials and ideas with coteachers"+"<br>"+
+                       "- Translate documents in English to Korean and vice versa"+"<br>"+
+                       "- Act as the translator between administrative staff and US military personnel"
+
     },
     {
       "employer" : "UNC School of Dentistry",
       "title" : "Materials Technician",
       "location" : "Chapel Hill, NC",
       "dates" : "07/2015-08/2015",
-      "description" : "Maintain dental equipment used by the School of Dentistry"
+      "description" : "- Sterilize and prepare dental cassettes and burrs according to protocol"+"<br>"+
+                      "- Maintain and organize laboratory and inventory"+"<br>"+
+                      "- Check in and check out dental equipment"
+
     },
     {
       "employer" : "UNC Gillings School of Global Public Health",
       "title" : "Interviewer",
       "location" : "Chapel Hill, NC",
       "dates" : "07/2014 ~ 08/2015",
-      "description" : [
-
-                      ]
+      "description" : "- Conduct research surveys funded by the FDA, CDC, and U.S. universities"+"<br>"+
+      "- Meet with university researchers to brainstorm survey questions and discuss interactions with respondents"+"<br>"+
+      "- Cold calling respondents on topics such as ancestry, genetics, tobacco policies, health"+"<br>"+
+      "- Data entry through the Blaise software"
     },
     {
       "employer" : "Residential Services, Inc.",
       "title" : "Direct Support Professional",
       "location" : "Chapel Hill, NC",
       "dates" : "07/2014 ~ 08/2015",
-      "description" : [
-
-                      ]
+      "description" : "- Provide support and supervision of residents with mental and intellectual disabilities"+"<br>"+
+      "- Answering phone calls, filing, emailing, and performing other clerical duties "+"<br>"+
+      "- Data entry through web-based developmental disabilities software, Therap Services"+"<br>"+
+      "- Facilitating and teaching daily routines in hygiene, finance management, nutrition education, meal preparation, educational development, and social skills"
     },
     {
       "employer" : "UNC School of Medicine: Thurston Bowles Lab",
       "title" : "Intern/Researcher",
       "location" : "Chapel Hill, NC",
       "dates" : "05/2013 ~ 08/2013",
-      "description" : [
-
-                      ]
+      "description" : "- Feed cells with ALI media (Air Liquid Interface)and cleanse with PBS (Phosphate buffered saline)"+"<br>"+
+      "- Autoclave biohazard waste, glassware"+"<br>"+
+      "- Prepare cell cultures with antibiotics and antifungals via pipetting"+"<br>"+
+      "- Experiment with various gels and buffer solution PBS to observe changes and assist postdoctoral mentor"+"<br>"+
+      "- Prepare ALI media with micropore filtration"
     },
 
   ]
@@ -80,39 +87,39 @@ var projects = {
   "projects" : [
     {
       "title" : "Random Quote Generator",
-      "date" :  "03/2016",
+      "dates" :  "03/2016",
       "description" : "Use third-party API to pull and display inspirational quotes",
-      "images" : ["url"]
+      "images" : ["url","url1"]
     },
     {
       "title" : "Weather App",
-      "date" :  "04/2016",
+      "dates" :  "04/2016",
       "description" : "Use third-party API to pull and display weather information based on geolocation",
-      "images" : ["url"]
+      "images" : ["url","url1"]
     },
     {
       "title" : "Wikipedia Searcher",
-      "date" :  "05/2016",
+      "dates" :  "05/2016",
       "description" : "Use Wikipedia API to search and display Wikipedia articles",
-      "images" : ["url"]
+      "images" : ["url","url1"]
     },
     {
       "title" : "Twitch TV Viewer",
-      "date" :  "05/2016",
+      "dates" :  "05/2016",
       "description" : "Use TwitchTV API to display data on streamers",
-      "images" : ["url"]
+      "images" : ["url","url1"]
     },
     {
       "title" : "Todo List",
-      "date" :  "09/2016",
+      "dates" :  "09/2016",
       "description" : "Todo list app which lets you add, edit, and delete entries",
-      "images" : ["url"]
+      "images" : ["url","url1"]
     },
     {
       "title" : "Park View App",
-      "date" :  "10/2016",
+      "dates" :  "10/2016",
       "description" : "Use NodeJS, Express, MongoDB, and JS to create, read, update, and destroy information on local parks.",
-      "images" : ["url"]
+      "images" : ["url","url1"]
     }
   ]
 };
@@ -123,15 +130,17 @@ var education = {
       "name" :"The University of North Carolina at Chapel Hill",
       "degree" : "BS, BA",
       "majors" : ["Biology", "History"],
-      "graduation" : "2015",
-      "location" : "Chapel Hill, NC"
+      "dates" : "2015",
+      "location" : "Chapel Hill, NC",
+      "url" : "http://www.unc.edu/"
     },
     {
       "name" :"Green Hope High School",
       "degree" : "Graduate",
       "majors" : ["Graduate"],
-      "graduation" : "2011",
-      "location" : "Cary, NC"
+      "dates" : "2011",
+      "location" : "Cary, NC",
+      "url" : "http://www.wcpss.net/greenhopehs"
     }
               ],
   "onlineCourses" : [
@@ -149,32 +158,26 @@ var education = {
     }
   ]
 };
-
+var replaceData = "%data%";
 //Display basic information about me
 bio.display = function () {
-  var formattedName=HTMLheaderName.replace("%data%", "Patrick Kim");
-  var formattedRole=HTMLheaderRole.replace("%data%", "Web Developer");
-  var formattedBioPic = HTMLbioPic.replace("%data%", "http://theeconomiccollapseblog.com/wp-content/uploads/2013/12/Bear-Market-Photo-by-Appalachian-Encounters-300x300.jpg");
-  var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-  var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-  var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-  var formattedLoc = HTMLlocation.replace("%data%", bio.contacts.location);
+  var formattedName=HTMLheaderName.replace(replaceData, "Patrick Kim"),
+   formattedRole=HTMLheaderRole.replace(replaceData, "Web Developer"),
+   formattedWelcomeMsg = HTMLwelcomeMsg.replace(replaceData, bio.welcomeMessage);
+   formattedBioPic = HTMLbioPic.replace(replaceData, "http://theeconomiccollapseblog.com/wp-content/uploads/2013/12/Bear-Market-Photo-by-Appalachian-Encounters-300x300.jpg");
+   formattedMobile = HTMLmobile.replace(replaceData, bio.contacts.mobile),
+   formattedEmail = HTMLemail.replace(replaceData, bio.contacts.email),
+   formattedGithub = HTMLgithub.replace(replaceData, bio.contacts.github),
+   formattedLoc = HTMLlocation.replace(replaceData, bio.contacts.location);
   $("#header").prepend(formattedRole);
   $("#header").prepend(formattedName);
   $("#header").append(formattedBioPic);
   $("#header").append(HTMLskillsStart);
-  $("#topContacts").append(formattedMobile);
-  $("#topContacts").append(formattedEmail);
-  $("#topContacts").append(formattedGithub);
-  $("#topContacts").append(formattedLoc);
-  // for (var i = 0; i < bio.contacts.length; i++){
-  //   if(bio.contacts.hasOwnProperty(i)) {
-  //     $("#topContacts").append(bio.contacts[i]);
-  //   }
-  // };
-if (bio.skills.length > 0) {
+  $("#header").append(formattedWelcomeMsg);
+  $("#topContacts, #footerContacts").append(formattedMobile, formattedEmail, formattedGithub, formattedLoc);
+  if (bio.skills.length > 0) {
   this.skills.forEach(function(skill){
-    var formattedSkill = HTMLskills.replace("%data%",skill);
+    var formattedSkill = HTMLskills.replace(replaceData,skill);
     $("#skills").append(formattedSkill);
   });
 }
@@ -185,60 +188,29 @@ bio.display();
 work.display = function() {
   $("#workExperience").append(HTMLworkStart);
 work.jobs.forEach(function(info){
-  var formattedTitle = HTMLworkTitle.replace("%data%", info.title);
-  var formattedEmployer = HTMLworkEmployer.replace("%data%", info.employer);
-  var formattedWorkDates = HTMLworkDates.replace("%data%", info.dates);
-  var formattedWorkDescription = HTMLworkDescription.replace("%data%", info.description);
-  $(".work-entry:last").append(formattedEmployer + formattedTitle + formattedWorkDates + formattedWorkDescription);
-});
+  var formattedTitle = HTMLworkTitle.replace(replaceData, info.title),
+   formattedEmployer = HTMLworkEmployer.replace(replaceData, info.employer),
+   formattedWorkDates = HTMLworkDates.replace(replaceData, info.dates),
+   formattedWorkDescription = HTMLworkDescription.replace(replaceData, info.description),
+   formattedWorkLocation = HTMLworkLocation.replace(replaceData,info.location);
+  $(".work-entry:last").append(formattedEmployer + formattedTitle + formattedWorkDates + formattedWorkLocation + formattedWorkDescription);
+  });
 };
 work.display();
-// for (job in work.jobs) {
-//
-//   var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-//   var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-//   var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-//   var formattedDescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
-//   $(".work-entry:last").append(formattedEmployer + formattedTitle);
-//   $(".work-entry:last").append(formattedWorkDates);
-//   $(".work-entry:last").append(formattedDescription);
-//
-// }
-
-
-
-// $(document).click(function(loc){
-//   var x = loc.pageX;
-//   var y = loc.pageY;
-//
-//   logClicks(x,y);
-// });
-// //
-// $("#main").append(internationalizeButton);
-// function inName(name) {
-//   var firstName,
-//   lastName,
-//   fullName;
-//   name = name.trim().split(" ");
-//   firstName = name[0].charAt(0).toUpperCase() + name[0].slice(1).toLowerCase();
-//   lastName = name[1].toUpperCase();
-//   fullName = firstName + " " + lastName;
-//   console.log(fullName);
-// }
 
 //Display info on projects
 projects.display = function() {
   $("#projects").append(HTMLprojectStart);
   var formattedProjectTitle,
-  formattedProjectDate,
+  formattedProjectDates,
   formattedProjectDescription,
   formattedProjectImage;
   this.projects.forEach(function(data){
-    formattedProjectTitle=HTMLprojectTitle.replace("%data%",data.title);
-    formattedProjectDate=HTMLprojectDates.replace("%data%",data.date);
-    formattedProjectDescription=HTMLprojectDescription.replace("%data%",data.description);
-    formattedProjectImage=HTMLprojectImage.replace("%data%",data.images);
-    $(".project-entry:last").append(formattedProjectTitle,formattedProjectDate, formattedProjectDescription, formattedProjectImage);
+    formattedProjectTitle=HTMLprojectTitle.replace(replaceData,data.title);
+    formattedProjectDates=HTMLprojectDates.replace(replaceData,data.dates);
+    formattedProjectDescription=HTMLprojectDescription.replace(replaceData,data.description);
+    formattedProjectImage=HTMLprojectImage.replace(replaceData,projects.images);
+    $(".project-entry:last").append(formattedProjectTitle,formattedProjectDates, formattedProjectDescription, formattedProjectImage);
   });
 
 };
@@ -250,26 +222,27 @@ education.display = function() {
   $("#education").append(HTMLonlineClasses);
   var schoolName,
   schoolDegree,
-  schoolDate,
+  schoolDates,
   schoolLocation,
   schoolMajor,
+  schoolURL,
   onlineTitle,
   onlineSchool,
   onlineDates,
   onlineURL;
   education.schools.forEach(function(data){
-    schoolName = HTMLschoolName.replace("%data%", data.name);
-    schoolDegree = HTMLschoolDegree.replace("%data%", data.degree);
-    schoolDate = HTMLschoolDates.replace("%data%", data.graduation);
-    schoolMajor = HTMLschoolMajor.replace("%data%", data.majors);
-    schoolLocation = HTMLschoolLocation.replace("%data%", data.location);
-    $(".education-entry").append(schoolName + schoolDegree + schoolDate + schoolMajor + schoolLocation);
+    schoolName = HTMLschoolName.replace(replaceData, data.name).replace("#", data.url);
+    schoolDegree = HTMLschoolDegree.replace(replaceData, data.degree);
+    schoolDates = HTMLschoolDates.replace(replaceData, data.dates);
+    schoolMajor = HTMLschoolMajor.replace(replaceData, data.majors);
+    schoolLocation = HTMLschoolLocation.replace(replaceData, data.location);
+    $(".education-entry").append(schoolName + schoolDegree + schoolDates + schoolMajor + schoolLocation);
   });
   education.onlineCourses.forEach(function(data){
-    onlineTitle = HTMLonlineTitle.replace("%data%", data.title);
-    onlineSchool = HTMLonlineSchool.replace("%data%", data.school);
-    onlineDates = HTMLonlineDates.replace("%data%", data.dates);
-    onlineURL = HTMLonlineURL.replace("%data%", data.url);
+    onlineTitle = HTMLonlineTitle.replace(replaceData, data.title);
+    onlineSchool = HTMLonlineSchool.replace(replaceData, data.school);
+    onlineDates = HTMLonlineDates.replace(replaceData, data.dates);
+    onlineURL = HTMLonlineURL.replace(replaceData, data.url);
 
     $(".online-entry").append(onlineTitle + onlineSchool + onlineDates + onlineURL);
   });
